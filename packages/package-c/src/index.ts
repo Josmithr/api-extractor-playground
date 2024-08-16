534d94d5a104decc675770c1be929e30b5e9ee16
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-08-16 17:43:51 +0000

    test: Add 3rd package to represent a minimally simplistic case.

---
+++ packages/package-c/src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * A simple test package with a single interface export.
+ * @packageDocumentation
+ */
+
+/**
+ * Foo
+ * @public
+ */
+export interface Foo {
+	/**
+	 * Bar
+	 */
+	bar: string;
+}
