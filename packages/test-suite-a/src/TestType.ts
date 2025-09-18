21b9f6419181bf04ace5a0202ef63bdb493b43a5
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-09-18 17:58:14 +0000

    test: Add some more type alias test cases

--- packages/test-suite-a/src/TestType.ts
+++ packages/test-suite-a/src/TestType.ts
@@ -31,6 +31,27 @@ export type TypeWithProperties = {
 };
 
 /**
+ * A test type with a construct signature.
+ */
+export type TypeWithConstructSignature = {
+	/**
+	 * Construct signature
+	 * @param input - A string input
+	 */
+	new (input: string): TypeWithConstructSignature;
+};
+
+/**
+ * A union type combining {@link TypeWithProperties} and {@link TypeWithConstructSignature}.
+ */
+export type UnionType = TypeWithProperties | TypeWithConstructSignature;
+
+/**
+ * An intersection type combining {@link TypeWithProperties} and {@link TypeWithConstructSignature}.
+ */
+export type IntersectionType = TypeWithProperties & TypeWithConstructSignature;
+
+/**
  * Test Mapped Type, using {@link TestEnum}
  *
  * @remarks Here are some remarks about the mapped type
